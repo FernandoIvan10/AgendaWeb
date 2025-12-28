8ba99125100eff8054d3b8272526140c2b0a97f3
FernandoIvan <fascenciocortes@gmail.com>
2025-12-27 20:25:00 -0600

    improved routes

--- server/routes/contacts.routes.js
+++ server/routes/contacts.routes.js
@@ -3,27 +3,15 @@ const { getContacts, addContact, updateContact, deleteContact } = require('../co
 const router = express.Router();
 
 // Get all contacts
-router.get(
-    '/contacts',
-    getContacts
-)
+router.get('/contacts', getContacts)
 
 // Add a new contact
-router.post(
-    '/contacts', 
-    addContact
-);
+router.post('/contacts', addContact);
 
 // Update a contact
-router.put(
-    '/contacts/:id',
-    updateContact
-);
+router.put('/contacts/:id', updateContact);
 
 // Delete a contact
-router.delete(
-    '/contacts/:id',
-    deleteContact
-);
+router.delete('/contacts/:id', deleteContact);
 
 module.exports = router;
