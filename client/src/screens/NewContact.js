cd35b801bdfb9ef76688c9ae973aca448847f7ce
FernandoIvan <fascenciocortes@gmail.com>
2025-12-31 17:15:10 -0600

    improved NewContact screen

--- client/src/screens/NewContact.js
+++ client/src/screens/NewContact.js
@@ -7,24 +7,41 @@ import { useNavigate } from 'react-router-dom';
 import './../assets/styles/screens/NewContact.css';
 
 // User can add new contact
-export default function NewContactPage(){
+export default function NewContactPage(){ 
+    // Navigation hook
     const navigate = useNavigate();
+    // contact state
     const [contact, setContact] = useState({ name: '', phone: '', description: '' });
+    const [saving, setSaving] = useState(false);
+    const [fetchError, setFetchError] = useState(null);
+    const [formError, setFormError] = useState(null);
+
     // method that saves a contact
-    const saveHandler = ()=>{
+    const saveHandler = async ()=>{
         // All fields must be filled in
-        if(!contact.name || !contact.phone || !contact.description){
-            alert('Please, fill in all fields')
+        if(
+            !contact.name.trim() 
+            || !contact.phone.trim() 
+            || !contact.description.trim()
+        ){
+            setFormError('Please, fill in all fields')
             return
         }
-        // New contact is added to contacts
-        axios.post('/api/contacts', contact)
-        .then(response => {
-            setContact({ name: "", phone: "", description: "" }); // Clear the form
-            alert("Contact saved successfully");
-            navigate('/contact-list');
-        })
-        .catch(err => console.error(err));
+
+        try {
+            if(saving) return; // Prevent multiple submissions
+            setSaving(true);
+            setFormError(null);
+            setFetchError(null);
+
+            await axios.post('/api/contacts', contact)
+            
+            navigate('/contact-list'); // Navigate to contact list
+        }catch(err){
+            setFetchError(err.message || 'An error occurred while saving the contact.');
+        } finally {
+            setSaving(false);
+        }
     }
 
     return (
@@ -32,10 +49,10 @@ export default function NewContactPage(){
             <div className="header">
                 <h1>Phone Book</h1>
                 <Link to="/contact-list">
-                    <RiContactsBook3Fill className="icon-list" />
+                    <RiContactsBook3Fill className="contact-form__list-button" />
                 </Link>
             </div>
-            <div className="contact-form_div">
+            <div className="contact-form__container">
                 <Input
                     label="Name: " 
                     id="contact-name" 
@@ -60,8 +77,16 @@ export default function NewContactPage(){
                     value={contact.description}
                     onChange={(e)=>setContact({...contact, description: e.target.value})}
                 />
-                <button id="button-save" className="contact-form_button" onClick={saveHandler}>Save</button>
+                <button 
+                    id="button-save" 
+                    className="contact-form__save-button" 
+                    onClick={saveHandler}
+                >
+                    Save
+                </button>
             </div>
+            {formError && <p className="error-message">{formError}</p>}
+            {fetchError && <p className="error-message">{fetchError}</p>}
         </div>
     )
 }
